0e30c87f7ca965671d550d3d3e17d8ca24cfd622
imperiumcuria-capital <imperiumcuria@gmail.com>
2026-04-27 22:35:01 -0400

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+title: ‘Imperium Curia Capital’,
+description: ‘A private investment platform for principal-led co-investment in companies approaching public market liquidity.’,
+}
+
+export default function RootLayout({ children }) {
+return (
+<html lang="en">
+<body>{children}</body>
+</html>
+)
+}
